f791536a7e07750f5e0e0fe56fe58093d294f77d
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-20 02:20:05 +0000

    index変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,12 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
 def new
   @books = []
 #   @booksの配列を空にする
@@ -18,7 +26,31 @@ def new
     # ヒットしたものをそれぞれ新しく作り、@booksの変数に<<で追加する
   end
 end
-  
+
+# def create
+# #   isbnの結果がなければ初めの1件を取得し1件もなければ作成
+#   @book = Book.find_or_initialize_by(isbn: params[:isbn])
+# #   データベースに保存されているかチェック
+#   unless @book.persisted?
+#     results = RakutenWebService::Books::Book.search(isbn: @book.isbn)
+#     @book.save
+#     redirect_to book_path(@book)
+#   end
+# end
+
+def create
+#   isbnの結果がなければ初めの1件を取得し1件もなければ作成
+    @book = Book.find_or_initialize_by(isbn: params[:isbn])
+
+#   データベースに保存されているかチェック
+    unless @book.persisted?
+      results = RakutenWebService::Books::Book.search(isbn: @book.isbn)
+      @book = Book.new(read(results.first))
+      @book.save
+      redirect_to book_path(@book)
+    end
+end
+
   private
   
   def read(result)
