cc60b82726c670149ff7b3f757e3de1fa727dddf
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-04-04 07:49:49 +0000

    課題２

--- Main.java
+++ Main.java
@@ -1,13 +1,24 @@
 public class Main {
   public static void main(String[] args) {
-    int a=5;
-    int b= 4;
-    int m = Math.max(a, b);
-    int min = Math.min(a,b);
-    System.out.println(m + min);
-    int r = new java.util.Random().nextInt(10);
-    System.out.println(r);
-    String s = new java.util.Scanner(System.in).nextLine();
-    int i = new java.util.Scanner(System.in).nextInt();
+    // int a=5;
+    // int b= 4;
+    // int m = Math.max(a, b);
+    // int min = Math.min(a,b);
+    // System.out.println(m + min);
+    // int r = new java.util.Random().nextInt(10);
+    // System.out.println(r);
+    // String s = new java.util.Scanner(System.in).nextLine();
+    // int i = new java.util.Scanner(System.in).nextInt();
+    System.out.println("ようこそ占いの館へ");
+    System.out.println("あなたの名前を入力してください");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("年齢を入力してください");
+    String ageString = new java.util.Scanner(System.in).nextLine();
+    int age = Integer.parseInt(ageString);
+    int fortune = new java.util.Random().nextInt(3);
+    fortune++;
+    System.out.println("占いの結果が出ました");
+    System.out.println(age + "歳の" + name + "さん、あなたの運記番号は" + fortune +"です");
+    System.out.println("1:2:3:4");
   }
 }
