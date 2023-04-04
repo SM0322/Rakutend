55dc8d4cd7a5a78cfe6495ef501bc76aa815dfe8
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-04-04 07:35:03 +0000

    Math.max()とMath.min()について

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int a=5;
+    int b= 4;
+    int m = Math.max(a, b);
+    int min = Math.min(a,b);
+    System.out.println(m + min);
+  }
+}
