c388de2c7fc47afc258287df42c5664167388299
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-04-04 07:58:00 +0000

    if文をswitch () case : break;で表記

--- Main.java
+++ Main.java
@@ -9,16 +9,31 @@ public class Main {
     // System.out.println(r);
     // String s = new java.util.Scanner(System.in).nextLine();
     // int i = new java.util.Scanner(System.in).nextInt();
-    System.out.println("ようこそ占いの館へ");
-    System.out.println("あなたの名前を入力してください");
-    String name = new java.util.Scanner(System.in).nextLine();
-    System.out.println("年齢を入力してください");
-    String ageString = new java.util.Scanner(System.in).nextLine();
-    int age = Integer.parseInt(ageString);
+    // System.out.println("ようこそ占いの館へ");
+    // System.out.println("あなたの名前を入力してください");
+    // String name = new java.util.Scanner(System.in).nextLine();
+    // System.out.println("年齢を入力してください");
+    // String ageString = new java.util.Scanner(System.in).nextLine();
+    // int age = Integer.parseInt(ageString);
+    // int fortune = new java.util.Random().nextInt(3);
+    // fortune++;
+    // System.out.println("占いの結果が出ました");
+    // System.out.println(age + "歳の" + name + "さん、あなたの運記番号は" + fortune +"です");
+    // System.out.println("1:2:3:4");
     int fortune = new java.util.Random().nextInt(3);
     fortune++;
-    System.out.println("占いの結果が出ました");
-    System.out.println(age + "歳の" + name + "さん、あなたの運記番号は" + fortune +"です");
-    System.out.println("1:2:3:4");
-  }
+    switch (fortune) {
+      case 1:
+          System.out.println(fortune);
+          break;
+      case 2:
+          System.out.println(fortune);
+          break;
+      case 3:
+          System.out.println(fortune);
+          break;
+      default:
+          System.out.println(fortune);
+    }
+    }
 }
