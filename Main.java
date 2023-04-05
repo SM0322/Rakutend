17fe873292dad6cc2b97179b93430903ad5873d5
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-04-05 01:06:33 +0000

    forでの集計

--- Main.java
+++ Main.java
@@ -127,8 +127,12 @@ public class Main {
     // }
     // System.out.println("ゲームを終了します");
     int[] scores = {20,30,40,50,60};
+    int sum = 0;
     for (int i = 0; i < scores.length; i++) {
       System.out.println(scores[i]);
+      sum += scores[i];
+      System.out.println(sum);
     }
+    
   }
 }
