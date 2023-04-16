1adbae552760ff98d33abe6dc7ada3f5f130de7c
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-04-16 23:55:42 +0000

    パッケージング

--- Calc.java
+++ Calc.java
@@ -1,8 +1,10 @@
+package calsapp.main;
+import calcapp.logics.*;
 public class Calc {
   public static void main(String[] args) {
     int a = 10; int b = 5;
     int total = CalcLogic.tasu(a, b);
-    int delta = CalcLogic.hiku(a, b);
+    int delta = calcapp.logics.CalcLogic.hiku(a, b);
     System.out.println(total);
     System.out.println(delta);
   }
