747c93c66bf65b2d5bfa36599937f9a90d2245f4
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-04-16 23:49:38 +0000

    cals calclogicに分割

--- Calc.java
+++ Calc.java
@@ -1,15 +1,9 @@
 public class Calc {
   public static void main(String[] args) {
     int a = 10; int b = 5;
-    int total = tasu(a, b);
-    int delta = hiku(a, b);
+    int total = CalcLogic.tasu(a, b);
+    int delta = CalcLogic.hiku(a, b);
     System.out.println(total);
     System.out.println(delta);
-  }
-  public static int tasu(int a, int b) {
-    return (a+b);
-  }
-  public static int hiku(int a, int b) {
-    return (a-b);
   }
 }
