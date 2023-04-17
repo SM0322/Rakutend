10d07448991f8f450a2542136e05a676f4dbbf6f
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-04-17 01:04:16 +0000

    8-3

--- Cleric.java
+++ Cleric.java
@@ -4,4 +4,10 @@ public class Cleric {
   final int MAXHP = 50;
   int mp;
   final int MAXMP =10;
+  public void selfAid() {
+    this.hp = MAXHP;
+    this.mp -= 5;
+    System.out.println(this.name + "はセルフエイドを唱えた!");
+    System.out.println(this.hp + "は最大まで回復した!");
+  }
 }
