919b6b9f23f9fa1e16ca41bca9b20b933d79466f
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-04-24 23:48:15 +0000

    Objectクラスで指定されたtoString()はインスタンスの中身を人が読めるような形で返すもの、なにも設定してないと番地が表示されるので、適切な形でオーバーライドする必要がある#

--- Hero.java
+++ Hero.java
@@ -36,9 +36,9 @@ public class Hero extends Character {
     m.hp -= 5;
     System.out.println("5ポイントのダメージ！");
   }
-  public String toString() {
-    return "名前：" + this.name + "/HP:" + this.hp;
-  }
+  // public String toString() {
+  //   return "名前：" + this.name + "/HP:" + this.hp;
+  // }
   public boolean equals(Object o) {
     if (this == o) {return true;}
     if (o instanceof Hero) {
