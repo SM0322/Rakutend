eb265e120cdd0ad94686a92c66aac196b961ff9b
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-04-24 04:14:04 +0000

    e.printStackTrace()でスタックトレースを表示

--- Main.java
+++ Main.java
@@ -13,6 +13,16 @@ public class Main {
   public static void main(String[] args) throws Exception {
     // Person p = new Person();
     // p.setAge(-128);
+    
+    try {
+      String s = null;
+      System.out.println(s.length());
+    } catch (NullPointerException e) {
+      System.out.println("NullPointerException例外をcatchしました");
+      System.out.println("--スタックトレース(ここから)--");
+      e.printStackTrace();
+      System.out.println("--スタックトレース(ここまで)--");
+    }
     try {
       throw new UnsupportedMusicFileException
         ("未対応のファイルです");
