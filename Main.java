22cbb3382356ecf6e3d62638d08e9cea2390e7a0
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-05-25 07:31:47 +0000

    switchと乱数を組み合わせたコード

--- Main.java
+++ Main.java
@@ -545,5 +545,25 @@ public class Main {
       }
       System.out.println(count);
     }
+    int[] seq = new int[10];
+    for (int i = 0; i < seq.length; i++) {
+      seq[i] = new java.util.Random().nextInt(4);
+    }
+    for (int i = 0; i < seq.length; i++) {
+      switch (seq[i]) {
+        case 0:
+          System.out.println("A");
+          break;
+        case 1:
+          System.out.println("T");
+          break;
+        case 2:
+          System.out.println("C");
+          break;
+        case 3:
+          System.out.println("G");
+          break;
+      }
+    }
   }
 }
