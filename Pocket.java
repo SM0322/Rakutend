9d8f38a2e5a3e151554ac8bdf7af66c151030ea3
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-05-25 00:32:21 +0000

    ジェネリクスを活用っしたコード

--- Pocket.java
+++ Pocket.java
@@ -1,9 +1,18 @@
-public class Pocket {
-  private Object data;
-  public void put(Object d) {
+// public class Pocket {
+//   private Object data;
+//   public void put(Object d) {
+//     this.data = d;
+//   }
+//   public Object get() {
+//       return this.data;
+//   }
+// }
+public class Pocket<E> {
+  private E data;
+  public void put(E d) {
     this.data = d;
   }
-  public Object get() {
+  public E get() {
       return this.data;
   }
 }
