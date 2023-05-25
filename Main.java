2ae90915bc29db280c3b69d2519748dd2e7ca756
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-05-25 07:09:26 +0000

    配列への数値の代入法

--- Main.java
+++ Main.java
@@ -530,7 +530,8 @@
 // }
 public class Main {
   public static void main(String[] args) {
-    int[] scores = new int[5];
+    int[] scores = {20, 30, 40, 50, 60};
     System.out.println(scores.length);
+    System.out.println(scores[0]);
   }
 }
