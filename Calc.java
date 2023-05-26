c606c2fa77e903edaed8ea8335602a6218fe9dfa
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-05-26 08:40:31 +0000

    パッケージ利用 :import

--- Calc.java
+++ Calc.java
@@ -1,10 +1,14 @@
 package calcapp.main;
+import calcapp.logics.CalcLogic;
 public class Calc {
   public static void main (String[] args) {
     int a = 10;
     int b = 5;
-    int total = calcapp.logics.CalcLogic.add(a,b);
-    int delta = calcapp.logics.CalcLogic.decrease(a,b);
+    // int total = calcapp.logics.CalcLogic.add(a,b);
+    int total = CalcLogic.add(a,b);
+    // import後はここまで短縮化
+    // int delta = calcapp.logics.CalcLogic.decrease(a,b);
+    int delta = CalcLogic.decrease(a,b);
     System.out.println("足すと" + total + "引くと" + delta);
   }
 }
