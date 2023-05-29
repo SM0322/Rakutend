cf171eefc442abf2592617e385bdc72d308aa74e
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-05-29 08:17:07 +0000

    8-3完成

--- Main.java
+++ Main.java
@@ -992,10 +992,32 @@ public class Main {
     // is.close();
     // os.flush();
     // os.close();
-    Socket sock = new Socket("smtp.example.com", 60025);
-    OutputStream os = sock.getOutputStream();
-    os.write("HELO smtp.example.com￥r￥n".getBytes());
-    os.flush();
-    sock.close();
+    // Socket sock = new Socket("smtp.example.com", 60025);
+    // OutputStream os = sock.getOutputStream();
+    // os.write("HELO smtp.example.com￥r￥n".getBytes());
+    // os.flush();
+    // sock.close();
+    String url = "https://api.github.com/users/miyabilink";
+    HttpClient client = HttpClient.newBuilder()
+      .version(Version.HTTP_1_1)
+      .followRedirects(Redirect.NORMAL)
+      .build();
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create(url))
+      .header("Accept", "application/vnd.github.v3+json")
+      .GET()
+      .build();
+    HttpResponse<String> response =
+      client.send(request, HttpResponse.BodyHandlers.ofstring());
+    String body = response.body();
+    int status = response.statusCode();
+    if (status == 200) {
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode root = mapper.readTree(body);
+      String blog = root.get("blog").textValue();
+      System.out.println(blog);
+    } else {
+      System.out.println("ERROR" + status);
+    }
   }
 }
