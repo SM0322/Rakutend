32cf9d3d39a773151b03acff895be814b3cfe68d
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-05-29 07:32:25 +0000

    8-1

--- Main.java
+++ Main.java
@@ -978,5 +978,16 @@ public class Main {
     // sock.getOutputStream().write("WELCOME".getBytes());
     // sock.getOutputStream().flush();
     // sock.close();
+    URL url = new URL("https://dokojava.jp/favicon.ico");
+    InputStream is = url.openStream();
+    OutputStream os = new FileOutputStream("dj.ico");
+    int i = is.read();
+    while (i != -1) {
+      os.write((byte)i);
+      i = is.read();
+    }
+    is.close();
+    os.flush();
+    os.close();
   }
 }
