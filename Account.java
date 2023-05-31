b508d75d13033dcebda7d17afd3be2301ba26e21
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-05-31 01:53:34 +0000

    テスト完了

--- Account.java
+++ Account.java
@@ -60,7 +60,7 @@
 public class Account {
   String owner;
   int zandaka;
-  public void Account(String owner, int zandaka) {
+  public Account(String owner, int zandaka) {
     owner = owner;
     zandaka = zandaka;
   }
